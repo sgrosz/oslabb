5886eafb8e5f103975bf388d25945784bd2ded51
Denise <denisen@kth.se>
2015-04-09 15:07:16 +0200

    Barnet sover nu

--- program.c
+++ program.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
 
 #define INPUT_LENGTH 80
 
 int main(int argc, char * argv[]){
 	char line[INPUT_LENGTH];
+	
+	pid_t pid = fork();
 
-	while(1){
-		printf("$ ");
-		if(!fgets(line, INPUT_LENGTH, stdin)) break;
-		printf("%s", line);
+	if(pid>0){
+	
+		while(1){
+			printf("$ ");
+			if(!fgets(line, INPUT_LENGTH, stdin)) break;
+			printf("%s", line);
+		}
+		return 0;
 	}
+	
+	if(pid==0){
+		sleep(10);
+	}
+	
 	return 0;
+
 }
