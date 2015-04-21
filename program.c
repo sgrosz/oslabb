4992fd39ef72789e0e3f912dfab1e530b5c121db
Sandra Grosz <sgrosz@kth.se>
2015-04-21 11:46:58 +0200

    Can almost change directories

--- program.c
+++ program.c
@@ -1,42 +1,58 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <string.h>
+#include <errno.h>
 
 #define INPUT_LENGTH 80
 #define MAX_ARGS 4
 
-void print_current_directory();
+void print_current_directory(char * path);
+void change_directory(char * path);
+
+char line[INPUT_LENGTH];
+char * home;
 
 int main(int argc, char * argv[]){
-	char line[INPUT_LENGTH];
-	char * args[MAX_ARGS];
-	char ** arg; 
+	char * path;
+	char * arg;
+	home = getenv("HOME");
+	path = home;
+	chdir(path);
 
 	while(1){
-		print_current_directory();
+		print_current_directory(path);
 		printf("$ ");
 		if(!fgets(line, INPUT_LENGTH, stdin)) break;
 
-		arg = args;
-        *arg++ = strtok(line," "); 
-        while ((*arg++ = strtok(NULL, " ")));
+        if(strncmp("cd", line, strlen("cd")) == 0){
+        	change_directory(&line[3]);
+        }
 
-        printf("%s\n", args[0]);
 	}
 	
 	return 0;
 
 }
 
-void print_current_directory(){
-	char * path;
+void print_current_directory(char * path){
 	char pathBuff[INPUT_LENGTH];
 
 	path = getcwd(pathBuff, INPUT_LENGTH);
+
 	if(path == NULL){
-        
+        fprintf(stderr, "%s\n", strerror(errno));
     }
 
     printf("%s", path);
+}
+
+void change_directory(char * path){
+	int ret;
+	printf("%s", path);
+	ret = chdir(path);
+
+	if(ret!=0){
+      perror("Error:");
+    }
 }
