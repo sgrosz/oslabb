221f889760f5b9b7e398030b697543604a51856b
Sandra Grosz <sgrosz@kth.se>
2015-04-21 12:38:14 +0200

    The cd command now works :smile:

--- program.c
+++ program.c
@@ -1,58 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
 #include <errno.h>
 
 #define INPUT_LENGTH 80
 #define MAX_ARGS 4
 
-void print_current_directory(char * path);
-void change_directory(char * path);
+void handle_command(char * command);
+void print_current_directory();
+void change_dir(char * path);
 
-char line[INPUT_LENGTH];
 char * home;
 
-int main(int argc, char * argv[]){
-	char * path;
-	char * arg;
+int main(){
+	char command[INPUT_LENGTH];
+	char * success;
+
 	home = getenv("HOME");
-	path = home;
-	chdir(path);
-
-	while(1){
-		print_current_directory(path);
-		printf("$ ");
-		if(!fgets(line, INPUT_LENGTH, stdin)) break;
-
-        if(strncmp("cd", line, strlen("cd")) == 0){
-        	change_directory(&line[3]);
-        }
-
-	}
+	change_dir(home);
 	
-	return 0;
+	while(1){
+		print_current_directory();
+		printf("$ ");
+		success = fgets(command, INPUT_LENGTH, stdin);
+		command[strlen(command) - 1] = '\0';
 
+		if(success == NULL){
+			printf("The line was not scanned properly.\n");
+		}
+
+		handle_command(command);
+	}
 }
 
-void print_current_directory(char * path){
-	char pathBuff[INPUT_LENGTH];
+/* Handles the command given in the prompt */
+void handle_command(char * command){
+	char * cmd;
+	char * args;
 
-	path = getcwd(pathBuff, INPUT_LENGTH);
+    cmd = strtok(command," "); 
 
-	if(path == NULL){
-        fprintf(stderr, "%s\n", strerror(errno));
+    if(strcmp(cmd, "cd") == 0){
+    	args = strtok(NULL, " ");
+
+    	if(args == NULL){
+    		change_dir(home);
+    	} else {
+    		change_dir(args);
+    	}
     }
-
-    printf("%s", path);
 }
 
-void change_directory(char * path){
-	int ret;
-	printf("%s", path);
-	ret = chdir(path);
+/* Gets the current working directory and prints it */
+void print_current_directory(){
+	char * path_ptr;
+	char path[INPUT_LENGTH];
 
-	if(ret!=0){
-      perror("Error:");
+	path_ptr = getcwd(path, INPUT_LENGTH);
+
+	if(path_ptr == NULL){
+        fprintf(stderr, "Error: %s\n", strerror(errno));
     }
+
+    printf("%s", path_ptr);
+}
+
+/* Changes the current directory with the path param */
+void change_dir(char * path){
+	int ok;
+	ok = chdir(path);
+
+	if(ok != 0){
+		fprintf(stderr, "Error: %s\n", strerror(errno));
+	}
 }
