691bfc51e2a77a6004b1a73ec9fda6f67834da9c
Sandra Grosz <sgrosz@kth.se>
2015-04-21 09:08:36 +0200

    Now prints current directory

--- program.c
+++ program.c
@@ -6,33 +6,37 @@
 #define INPUT_LENGTH 80
 #define MAX_ARGS 4
 
+void print_current_directory();
+
 int main(int argc, char * argv[]){
 	char line[INPUT_LENGTH];
 	char * args[MAX_ARGS];
 	char ** arg; 
-	
-	pid_t pid = fork();
 
-	if(pid>0){
-	
-		while(1){
-			printf("$ ");
-			if(!fgets(line, INPUT_LENGTH, stdin)) break;
-			printf("%s", line);
+	while(1){
+		print_current_directory();
+		printf("$ ");
+		if(!fgets(line, INPUT_LENGTH, stdin)) break;
 
-			arg = args;
-	        *arg++ = strtok(line," "); 
-	        while ((*arg++ = strtok(NULL, " ")));
+		arg = args;
+        *arg++ = strtok(line," "); 
+        while ((*arg++ = strtok(NULL, " ")));
 
-	        printf("%s\n", args[0]);
-		}
-		return 0;
-	}
-	
-	if(pid==0){
-		sleep(10);
+        printf("%s\n", args[0]);
 	}
 	
 	return 0;
 
+}
+
+void print_current_directory(){
+	char * path;
+	char pathBuff[INPUT_LENGTH];
+
+	path = getcwd(pathBuff, INPUT_LENGTH);
+	if(path == NULL){
+        
+    }
+
+    printf("%s", path);
 }
