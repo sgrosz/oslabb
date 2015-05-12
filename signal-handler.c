38d36b413e7ae8d87be7a4643823e966b549c25d
Sandra Grosz <sgrosz@kth.se>
2015-05-12 16:52:53 +0200

    Added files

---
+++ signal-handler.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+#include "cd.h"
+
+void background_terminated();
+void setup_child_handler();
+void setup_interrupt_handler();
+void interrupt_handler(int signum);
+
+void background_terminated(){
+	pid_t p;
+	int status;
+
+	if((p = waitpid(-1, &status, WNOHANG)) > 0){
+		printf("Background process %d terminated\n", p);
+	}
+}
+
+void setup_child_handler(){
+	/* Establish handler. */
+	struct sigaction child;
+	child.sa_handler = &background_terminated;
+	child.sa_flags = SA_RESTART;
+	sigemptyset(&child.sa_mask);
+
+	sigaction(SIGCHLD, &child, 0);
+}
+
+void setup_interrupt_handler(){
+	/* Establish handler. */
+	struct sigaction interrupt;
+	interrupt.sa_handler = &interrupt_handler;
+	interrupt.sa_flags = SA_RESTART;
+	sigemptyset(&interrupt.sa_mask);
+
+	sigaction(SIGINT, &interrupt, 0);
+}
+
+void interrupt_handler(int signum){
+	/*DO NOTHING*/
+
+	fflush(stdin);
+
+	printf("\n");
+	print_current_directory();
+	printf("> ");
+
+	fflush(stdout);
+}
---
+++ signal-handler.h
@@ -0,0 +1,4 @@
+void background_terminated();
+void setup_child_handler();
+void setup_interrupt_handler();
+void interrupt_handler(int signum);
